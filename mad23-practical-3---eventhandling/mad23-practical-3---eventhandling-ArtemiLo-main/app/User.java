c2d9ce511dcbd1fcdbef1d97afdecf1416202266
Zacharychinnn <130948562+Zacharychinnn@users.noreply.github.com>
2023-09-05 22:26:43 +0800

    Add files via upload

---
+++ mad23-practical-3---eventhandling/mad23-practical-3---eventhandling-ArtemiLo-main/app/User.java
@@ -0,0 +1,14 @@
+public class User {
+    public String name;
+    public String description;
+    public int ID;
+    public boolean followed;
+
+    public User (string n, string d, int i, boolean f)
+    {
+        this.n = name;
+        this.d = description;
+        this.i = ID;
+        this.f = followed;
+    }
+}
